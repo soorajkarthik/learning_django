e73025e5db84c7341faf57f2bc2744aa703b0b73
Sooraj Karthik <soorajkarthik15@gmail.com>
2020-12-04 11:43:38 -0600

    Reformat

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -10,10 +10,12 @@ class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),]
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
     inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render, get_object_or_404
-from django.template import loader
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from .models import Question, Choice
 
 # Get questions and display them
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 # Show specific question and choices
+
+
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404('Question does not exist')
-    return render(request, 'polls/detail.html', { 'question': question })
+    return render(request, 'polls/detail.html', {'question': question})
 
 # Get question and display results
+
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', { 'question': question })
+    return render(request, 'polls/results.html', {'question': question})
 
 # Vote for a question choice
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
